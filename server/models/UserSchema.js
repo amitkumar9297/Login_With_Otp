127a94616992e9585c5073c96e4e34a39bc793b0
Amit Kumar <amitkumar790194@gmail.com>
2024-05-23 01:34:32 +0530

    userSchema and userOTP model are created

---
+++ server/models/UserSchema.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    fname: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Not Valid Email")
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [
+        {
+            token: {
+                type: String,
+                required: true,
+            }
+        }
+    ]
+});
+
+
+// schema model created
+const users = new mongoose.model("users", userSchema);
+
+module.exports = users;
